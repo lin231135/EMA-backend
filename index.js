19013ab4a3100e0e116d35443e50b5f1c040e17e
lin231135 <lin231135@uvg.edu.gt>
2025-07-18 03:02:27 -0600

    EMA-14 Instalar dependencias iniciales

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import routes from './src/routes/index.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
+});
