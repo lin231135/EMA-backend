e315ca2bc96831932b65c9b79702aafe416c2936
Nadissa <vel23764@uvg.edu.gt>
2025-10-15 02:31:23 -0600

    EMA-128 Formulario para tomar accion

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ import adminRoutes from './admins.routes.js';
 import supRoutes from './sup.routes.js';
 import notificationRoutes from './notifications.routes.js';
 import contact from './contact.routes.js';
+import joinTeam from './join.team.routes.js';
 
 const router = Router();
 
@@ -23,6 +24,7 @@ router.use('/admins', adminRoutes)  // /api/admins/*
 router.use('/sup', supRoutes)  // /api/sup/*
 router.use('/notifications', notificationRoutes)  // /api/notifications/*
 router.use('/contact', contact)  // /api/contact/*
+router.use('/join-team', joinTeam)  // /api/join-team/* 
 
 
 export default router;
