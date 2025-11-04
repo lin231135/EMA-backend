c21a4c0d1c3787042e2888fe6695b42bbcfc6e44
Sebas Túnchez <tun231359@uvg.edu.gt>
2025-11-04 13:59:08 -0600

    EMA-UPDATE Base de datos

--- src/db/06_data.sql
+++
@@ -1,9 +0,0 @@
--- Crear un usuario padre
-INSERT INTO "User"(name, last_name, email, phone, password, role, is_active)
-VALUES ('David', 'Chet', 'david@ema.com', '555-0000', crypt('test1234', gen_salt('bf', 12)) , 'padre', TRUE);
-
--- Crea hijos (tabla Kid)
-INSERT INTO Kid(name, birth_date, parent_id)
-VALUES 
-  ('Daniel Chet', '2014-05-10', (SELECT id FROM "User" WHERE email='david@ema.com')),
-  ('Sofía Chet', '2011-11-22', (SELECT id FROM "User" WHERE email='david@ema.com'));
